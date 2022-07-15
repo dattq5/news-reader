7256ce7fb0c2e6fc163d7a0e52c6c8915ac7b975
Dat Tran <tranquocdat311090@gmail.com>
2022-07-16 02:20:01 +0700

    create gh-pages branch for publishing purpose

--- src/common/api.ts
+++
@@ -1,9 +0,0 @@
-export const BASE_PATH = import.meta.env.VITE_BASE_PATH;
-export const API_KEY = import.meta.env.VITE_API_KEY;
-
-export const API = {
-  getArticles: ({page = 1, pageSize = 20, source = ''}) => `
-      ${BASE_PATH}/top-headlines?pageSize=${pageSize}&page=${page}${source ? '&sources=' + source : '&country=au'}&apiKey=${API_KEY}
-    `,
-  getSources: () => `${BASE_PATH}/top-headlines/sources?apiKey=${API_KEY}`
-}
